12e6c3f769cd8a4f38a2a92568760fac94363028
JonElliot Antognoni <jonantognoni@hotmail.com>
2021-03-05 14:58:36 -0500

    setup typescript front-end

---
+++ front-end/src/App.tsx
@@ -0,0 +1,21 @@
+// React imports
+
+import {
+	Redirect,
+	Route,
+	BrowserRouter as Router,
+	Switch,
+} from "react-router-dom";
+
+function App()
+{
+	return (
+		<Router>
+			<Switch>
+				
+			</Switch>
+		</Router>
+	);
+}
+
+export default App;
