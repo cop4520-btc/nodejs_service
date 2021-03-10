4c26ed40793e98cbc99079c0f853ea4e2b16e28d
JonElliot Antognoni <jonantognoni@hotmail.com>
2021-03-10 02:43:08 -0500

    fixed issue with not being able to use request.body

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,13 @@
-import express from 'express';
-import * as dotenv from 'dotenv';
+import body_parser from "body-parser";
+import * as dotenv from "dotenv";
+import express from "express";
 
 // api endpoints
 import { login } from "./api/login";
 
 const app = express();
 dotenv.config();
+app.use(body_parser);
 const PORT = process.env.PORT || 5000;
 
 app.get('/', (_, res) => {
