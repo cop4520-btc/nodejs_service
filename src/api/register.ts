2c6c7f790a9fd2617164045446e10d971f734cbe
JonElliot Antognoni <jonantognoni@hotmail.com>
2021-03-11 16:51:07 -0500

    Documented types and set up return package in register endpoint

--- src/api/register.ts
+++ src/api/register.ts
@@ -4,5 +4,19 @@ import { Request, Response } from "express";
 
 export async function register(request: Request, response: Response, next: CallableFunction)
 {
-	
+	let returnPackage: RegisterReturnPackage = {
+		success: false,
+		error: ""
+	};
+
+	// configure mysql connection data
+	const connectionData: mysql.ConnectionConfig = {
+		host: process.env.RDS_HOSTNAME,
+		user: process.env.RDS_USERNAME,
+		password: process.env.RDS_PASSWORD,
+		port: Number(process.env.RDS_PORT),
+		database: "btc"
+	};
+
+	const connection: mysql.Connection = mysql.createConnection(connectionData);
 }
--- src/types/registerTypes.ts
+++ src/types/registerTypes.ts
@@ -1,3 +1,7 @@
+/**
+ * Only return success and error string since website will redirect
+ * users to login after the registration is complete
+ */
 export interface RegisterReturnPackage
 {
 	success: boolean,
