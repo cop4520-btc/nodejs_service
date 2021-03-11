cdf7f817d815670583d5837e521436eee78ae0be
JonElliot Antognoni <jonantognoni@hotmail.com>
2021-03-11 14:44:44 -0500

    Implemented and tested login api

--- src/api/login.ts
+++ src/api/login.ts
@@ -1,7 +1,6 @@
 import * as mysql from "mysql";
-import { LoginData, LoginReturnPackage } from "../types/loginTypes";
+import { LoginData, LoginReturnPackage, UserData } from "../types/loginTypes";
 import { Request, Response } from "express";
-import { sqlQueryResultHandler } from "../util/sqlQueryResultHandler";
 import { resolve } from "node:path";
 
 export async function login(request: Request, response: Response, next: CallableFunction): Promise<void>
@@ -55,7 +54,43 @@ export async function login(request: Request, response: Response, next: Callable
 	// query the database for the Users with matching credentials
 	try
 	{
-		let result = connection.query(queryString);
+		connection.query(queryString, (error: string, rows: Array<Object>) => {
+			if (error)
+			{
+				returnPackage.error = error;
+				response.json(returnPackage);
+				response.status(500);
+				response.send();
+				return;
+			}
+
+			// return with error if no user was found
+			if (rows.length < 1)
+			{
+				returnPackage.error = "Username or Password incorrect";
+				response.json(returnPackage);
+				response.status(404);
+				response.send();
+				return;
+			}
+
+			let userData: UserData = JSON.parse(JSON.stringify(rows[0]));
+
+			// transfer query data to returnPackage fields
+			returnPackage.userID = userData.ID;
+			returnPackage.username = userData.username;
+			returnPackage.firstname = userData.firstname;
+			returnPackage.lastname = userData.lastname;
+			returnPackage.address = userData.address;
+			returnPackage.lastUpdate = userData.lastUpdate;
+			returnPackage.balance = userData.balance;
+			returnPackage.spent = userData.spent;
+
+			returnPackage.success = true;
+			response.json(returnPackage);
+			response.status(200);
+			response.send();
+		});
 	}
 	catch (e)
 	{
@@ -66,12 +101,6 @@ export async function login(request: Request, response: Response, next: Callable
 		connection.end();
 		return;
 	}
-
-	console.log(queryString);
-
-	response.json(returnPackage);
-	response.status(200);
-	response.send();
 
 	connection.end();
 }
--- src/types/loginTypes.ts
+++ src/types/loginTypes.ts
@@ -1,13 +1,28 @@
-export interface LoginData {
+export interface LoginData
+{
 	username: string,
 	password: string
 };
 
-export interface LoginReturnPackage {
+export interface LoginReturnPackage
+{
 	success: boolean,
 	error: string,
 	userID: number,
 	username: string,
+	firstname: string,
+	lastname: string,
+	address: string,
+	lastUpdate: number,
+	balance: number,
+	spent: number
+};
+
+export interface UserData
+{
+	ID: number,
+	username: string,
+	password: string,
 	firstname: string,
 	lastname: string,
 	address: string,
