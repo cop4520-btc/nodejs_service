1537d4bc12c5e84ebfe9874f617bb3e6941e2185
JonElliot Antognoni <jonantognoni@hotmail.com>
2021-03-12 22:29:43 -0500

    Isolated userData to its own type and added file for query return types

---
+++ src/types/queryReturnTypes.ts
@@ -0,0 +1,12 @@
+export interface UsersQueryReturn
+{
+	id: number,
+	username: string,
+	password: string,
+	firstname: string,
+	lastname: string,
+	address: string,
+	lastUpdate: number,
+	balance: number,
+	spent: number
+};
---
+++ src/types/userTypes.ts
@@ -0,0 +1,11 @@
+export interface UserData
+{
+	userID: number,
+	username: string,
+	firstname: string,
+	lastname: string,
+	address: string,
+	lastUpdate: number,
+	balance: number,
+	spent: number
+}
